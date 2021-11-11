c976eaddef1cde833b77b46ea7b5eb5b6e29fda8
victordalet <victordalet@protonmail.com>
2021-11-11 11:34:32 +0100

    fin de liste.py

--- list/liste.py
+++ list/liste.py
@@ -1,3 +1,4 @@
+import random,csv
 def longeurListe(liste):
 	c = 0
 	for i in liste:
@@ -84,13 +85,19 @@ def calculeEtendue(liste):
 #########################################################
 
 def ajouterElement(liste,element):
-	liste += element
+	liste += [element]
 	return liste
 
 #########################################################
 
 def supprimerElement(liste,rang= -1):
-	del liste[rang]
+	if rang == -1:
+		rang = longeurListe(liste) -1
+	dic = couperListe(liste)
+	liste = supprimerListe(liste)
+	for key,value in dic.items():
+		if key != str(rang):
+			liste = ajouterElement(liste,value)
 	return liste
 
 #########################################################
@@ -107,20 +114,21 @@ def recupererMax(liste):
 
 #########################################################
 
-def fromCvsToList(liste,url):
-	return liste
+def fromCvsToList(url):
+	contenu = csv.reader(open(url,"rb"))
+	return contenu
 
 
 #########################################################
 
-def aleatoire(min,max):
-	return n
+def aleatoire():
+	return nombre
 
 
 #########################################################
 
-def randomList():
-	liste = [aleatoire(0,50) for i in range(aleatoire(0,50))]
+def randomList(mini=0,maxi=100):
+	liste = [random.randint(mini,maxi) for i in range(random.randint(mini,maxi))]
 	return liste
 
 
@@ -135,100 +143,8 @@ def couperListe(liste):
 	dic_liste = {}
 	for i in range(longeurListe(liste)):
 		dic_liste[str(i)] = liste[i]
+	print(dic_liste)
 	return dic_liste
 
 
-def test():
-	print(supprimerElement([5,9,7,2],1))
-
-
-	def doc() : 	
-		liste.longeurListe(ma_liste)
-		"""
-		entrée : une liste
-		sortie : nombre d'éléments de la liste
-		"""
-
-		liste.ordreCroissant(ma_liste)
-		"""
-		entrée : une liste d' int ou float
-		sortie : la liste triée dans l'ordre croissant
-		"""
-
-		liste.ordreDecroissant(ma_liste)
-		"""
-		entrée : une liste d' int ou float
-		sortie : la liste triée dans l'ordre décroissant
-		"""
-
-		liste.calculeMoyenne(ma_liste)
-		"""
-		entrée : une liste d' int ou float
-		sortie : la moyenne de la liste
-		"""
-
-		liste.calculeEtendue(ma_liste)
-		"""
-		entrée : une liste d' int ou float
-		sortie : l'étendue de la liste
-		"""
-
-		liste.calculeMediane(ma_liste)
-		"""
-		entrée : une liste d' int ou float
-		sortie : la mediane de la liste
-		"""
-
-		liste.ajouterElement(ma_liste,element)
-		"""
-		entrée : une liste , un élément
-		sortie : la liste avec un nouvelle élément
-		"""
-
-		liste.supprimerElement(ma_liste,rang)
-		"""
-		entrée : une liste , le rang d'une liste (prédéfinie au dernier élément)
-		sortie : la liste avec l'élément en moins
-		"""
-
-		liste.recupererMin(ma_liste)
-		"""
-		entrée : une liste d' int ou float
-		sortie : l'élément le plus petit de la liste
-		"""
-
-		liste.recupererMax(ma_liste)
-		"""
-		entrée : une liste d' int ou float
-		sortie : l'élément le plus grand de la liste
-		"""
-
-		liste.fromCvsToList(url)
-		"""
-		entrée : l'url d'un fichier cvs 
-		sortie : la liste du fichier cvs
-		"""
-
-		liste.randomList()
-		"""
-		entrée : rien 
-		sortie : une liste avec des nombres aléatoires et de rangs aléatoires  
-		"""
-
-
-		liste.supprimerListe(ma_liste)
-		"""
-		entrée : une liste 
-		sortie : la liste vidée
-		"""
-
-		liste.couperListe(ma_liste)
-		"""
-		entrée : une liste
-		sortie : un dictionnaire avec en valeur le rang et en valeur la valeur du rang 
-		"""
-
-	
-
-
-test()
+print(supprimerElement([5,4,8,1]))
---
+++ list/main.py
@@ -0,0 +1,93 @@
+import liste
+
+
+def main():
+	ma_liste = [4,5,6,2,1,9]
+	element = 9
+
+	print(liste.longeurListe(ma_liste))
+	"""
+	entrée : une liste
+	sortie : nombre d'éléments de la liste
+	"""
+
+	print(liste.ordreCroissant(ma_liste))
+	"""
+	entrée : une liste d' int ou float
+	sortie : la liste triée dans l'ordre croissant
+	"""
+
+	print(liste.ordreDecroissant(ma_liste))
+	"""
+	entrée : une liste d' int ou float
+	sortie : la liste triée dans l'ordre décroissant
+	"""
+
+	print(liste.calculeMoyenne(ma_liste))
+	"""
+	entrée : une liste d' int ou float
+	sortie : la moyenne de la liste
+	"""
+
+	print(liste.calculeEtendue(ma_liste))
+	"""
+	entrée : une liste d' int ou float
+	sortie : l'étendue de la liste
+	"""
+
+	print(liste.calculeMediane(ma_liste))
+	"""
+	entrée : une liste d' int ou float
+	sortie : la mediane de la liste
+	"""
+
+	print(liste.ajouterElement(ma_liste,element))
+	"""
+	entrée : une liste , un élément
+	sortie : la liste avec un nouvelle élément
+	"""
+
+	print(liste.supprimerElement(ma_liste,rang))
+	"""
+	entrée : une liste , le rang d'une liste (prédéfinie au dernier élément)
+	sortie : la liste avec l'élément en moins
+	"""
+
+	print(liste.recupererMin(ma_liste))
+	"""
+	entrée : une liste d' int ou float
+	sortie : l'élément le plus petit de la liste
+	"""
+
+	print(liste.recupererMax(ma_liste))
+	"""
+	entrée : une liste d' int ou float
+	sortie : l'élément le plus grand de la liste
+	"""
+
+	print(liste.fromCvsToList(url))
+	"""
+	entrée : l'url d'un fichier cvs 
+	sortie : la liste du fichier cvs
+	"""
+
+	print(liste.randomList())
+	"""
+	entrée : rien 
+	sortie : une liste avec des nombres aléatoires et de rangs aléatoires  
+	"""
+
+
+	print(liste.supprimerListe(ma_liste))
+	"""
+	entrée : une liste 
+	sortie : la liste vidée
+	"""
+
+	print(liste.couperListe(ma_liste))
+	"""
+	entrée : une liste
+	sortie : un dictionnaire avec en valeur le rang et en valeur la valeur du rang 
+	"""
+
+main()
