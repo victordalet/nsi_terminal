01867b908c8a21a623897c0cf34358fa3b7ca0f9
victordalet <victordalet@protonmail.com>
2022-02-10 14:13:30 +0100

    poo

--- perso/code de la route/main.py
+++ perso/code de la route/main.py
@@ -1,17 +1,17 @@
-from tkinter import *
+import tkinter
 from py.input import * 
 from py.data import *
 from py.load import *
 
 def main():
-	W = Tk()
-	W.geometry('300x110')
-	W.title("CODE DE LA ROUTE")
-	W.configure(bg="#141414")
-	bttn(W,0,0,"LANCER","#ffcc66","#141414",load)
-	bttn(W,0,37,"OPTIONS","#25dae9","#141414")
-	bttn(W,0,74,"QUITTER","#f86263","#141414",W.destroy)
-	W.mainloop()
+	screen = tkinter.Tk()
+	screen.geometry('300x110')
+	screen.title("CODE DE LA ROUTE")
+	screen.configure(bg="#141414")
+	bttn(screen,0,0,"LANCER","#ffcc66","#141414",questionnaire)
+	bttn(screen,0,37,"OPTIONS","#25dae9","#141414")
+	bttn(screen,0,74,"QUITTER","#f86263","#141414",screen.destroy)
+	screen.mainloop()
 
 
 main()
--- perso/code de la route/py/load.py
+++ perso/code de la route/py/load.py
@@ -1,53 +1,104 @@
-from tkinter import *
+import tkinter
 from py.data import *
 from py.input import *
-from py.score import *
 import random
 import pyttsx3
 
-def load():
-	W = Tk()
-	W.geometry(str(W.winfo_screenwidth())+'x'+str(W.winfo_screenheight()))
-	W.title("CODE DE LA ROUTE")
-	W.configure(bg="#141414")
-	data = import_data('data/questions.json')
-	list_question = random_question(data)
-	display(W,list_question,data)
-	W.mainloop()
 
 
-def random_question(data):
-	return random.choice(data)
+class questionnaire:
 
-def display(W,list_question,data):
-	question = list_question[0]
-	choice1 = list_question[1]
-	choice2 = list_question[2]
-	choice3 = list_question[3]
-	choice4 = list_question[4]
-	good = list_question[5]
-	url_image = 'assets/images/'+str(take_len(list_question,data))+'.png'
-	label(W,question,"#d5d5d5","#f86263")
-	talk(list_question[0])
-	#picture(W,url_image)
-	bttn(W,W.winfo_screenwidth()*1/5,W.winfo_screenheight()*2/3,choice1,"#d5d5d5","#f86263",valide1)
-	bttn(W,W.winfo_screenwidth()*3/5,W.winfo_screenheight()*2/3,choice2,"#d5d5d5","#f86263",valide2)
-	bttn(W,W.winfo_screenwidth()*1/5,W.winfo_screenheight()*3/4,choice3,"#d5d5d5","#f86263",valide3)
-	bttn(W,W.winfo_screenwidth()*3/5,W.winfo_screenheight()*3/4,choice4,"#d5d5d5","#f86263",valide4)
+	def __init__(self):
+		self.screen = tkinter.Tk()
+		self.screen.geometry(str(self.screen.winfo_screenwidth())+'x'+str(self.screen.winfo_screenheight()))
+		self.screen.title("CODE DE LA ROUTE")
+		self.screen.configure(bg="#141414")
+		self.data = import_data('data/questions.json')
+		self.list_question = self.random_question()
+		self.display()
+		self.screen.mainloop()
+		self.list_answers = []
+
+	def random_question(self):
+		return random.choice(self.data)
+
+	def display(self):
+		question = self.list_question[0]
+		choice1 = self.list_question[1]
+		choice2 = self.list_question[2]
+		choice3 = self.list_question[3]
+		choice4 = self.list_question[4]
+		good = self.list_question[5]
+		url_image = 'assets/images/'+str(self.take_len())+'.png'
+		label(self.screen,question,"#d5d5d5","#f86263")
+		self.talk(self.list_question[0])
+		#picture(W,url_image)
+		bttn(self.screen,self.screen.winfo_screenwidth()*1/5,self.screen.winfo_screenheight()*2/3,choice1,"#d5d5d5","#f86263",self.valide1)
+		bttn(self.screen,self.screen.winfo_screenwidth()*3/5,self.screen.winfo_screenheight()*2/3,choice2,"#d5d5d5","#f86263",self.valide2)
+		bttn(self.screen,self.screen.winfo_screenwidth()*1/5,self.screen.winfo_screenheight()*3/4,choice3,"#d5d5d5","#f86263",self.valide3)
+		bttn(self.screen,self.screen.winfo_screenwidth()*3/5,self.screen.winfo_screenheight()*3/4,choice4,"#d5d5d5","#f86263",self.valide4)
 
 
 
-def take_len(list_question,data):
-	for i in range(len(data)):
-		if data[i] == list_question:
-			return i+1
+	def take_len(self):
+		for i in range(len(self.data)):
+			if self.data[i] == self.list_question:
+				self.del_question(self.data[i])
+				return i+1
 
-def talk(text):  
-	s = pyttsx3.init()    
-	s.say(text)  
-	s.runAndWait() 
+	def del_question(self,rang):
+		new_list = []
+		for i in self.data:
+			if i != rang:
+				new_list += i
+		self.data = new_list
 
-def new_question():
-	data = import_data('data/questions.json')
-	list_question = random_question(data)
-	display(W,list_question,data)
+	def talk(self,text):  
+		s = pyttsx3.init()    
+		s.say(text)  
+		s.runAndWait() 
+
+	def valide1(self):
+		reponse = import_data('data/questions.json')[-1]
+		self.end_game(reponse)
+		if reponse == 1:
+			self.list_answers += True
+		else : 
+			self.list_answers += False
+		self.new_question()
+
+	def valide2(self):
+		reponse = import_data('data/questions.json')[-1]
+		self.end_game(reponse)
+		if reponse == 2:
+			self.list_answers += True
+		else : 
+			self.list_answers += False
+		self.new_question()
+
+	def valide3(self):
+		reponse = import_data('data/questions.json')[-1]
+		self.end_game(reponse)
+		if reponse == 3:
+			self.list_answers += True
+		else : 
+			self.list_answers += False
+		self.new_question()
+
+	def valide4(self):
+		reponse = import_data('data/questions.json')[-1]
+		self.end_game(reponse)
+		if reponse == 4:
+			self.list_answers += True
+		else : 
+			self.list_answers += False
+		self.new_question()
+
+	def end_game(self,list):
+		if len(list) == 39:
+			for i in range(len(self.list_answers)):
+				print("questions {} la réponse étais {}".format(i+1, self.list_answers[i]))
+			list_score = import_data('data/score.json')	
+			list_score += [self.list_answers]
+			give_data('data/score.json',list_score)
+
