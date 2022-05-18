bb344ad9bc9c417c5777b1c8854c0bbee3dc1f57
victordalet <victordalet@protonmail.com>
2022-05-18 10:40:58 +0200

    fin jeu manque design

--- perso/pn/main.py
+++ perso/pn/main.py
@@ -59,7 +59,7 @@ def give_data(url,data):
 
 
 class game:
-    def __init__(self):
+    def __init__(self,mode=0):
         self.fenetre=Tk()
         self.Fond=Canvas(self.fenetre,bg='white',width=800,height=600)
         self.photoballe = []
@@ -76,10 +76,14 @@ class game:
         self.position = 300
         self.vitesse = 20
         self.win = 3
+        self.mode = mode
+        self.droit = True
+        self.gauche = True
         self.time_d = round(time.time())
-        self.btn_time = bttn(self.fenetre,1,1,str(round(time.time())-self.time_d),"#eeeeee","#000000")
+        if mode: self.btn_time = bttn(self.fenetre,1,1,str(round(time.time())-self.time_d),"#eeeeee","#000000")
+        else : self.btn_time = bttn(self.fenetre,1,1,str(60-(round(time.time())-self.time_d)),"#eeeeee","#000000")
         self.btn_heart = bttn(self.fenetre,750,1,str(self.win),"#eeeeee","#000000")
-        bttn(self.fenetre,680,1,"record : "+str(import_data('score.json')),"#eeeeee","#000000")
+        if self.mode : bttn(self.fenetre,680,1,"record : "+str(import_data('score.json')),"#eeeeee","#000000")
         self.picture('NoelG.gif')
         bttn(self.fenetre,800/2-40,20,"<","#eeeeee","#000000",self.xless)
         bttn(self.fenetre,800/2+25,20,">","#eeeeee","#000000",self.xmore)
@@ -87,15 +91,27 @@ class game:
         self.animation()
         self.fenetre.mainloop()
 
-    def xmore(self):
+    def xmore(self):    
         if self.win != False:
-            self.position += self.vitesse
-            self.picture('NoelD.gif')
+            if self.droit:
+                self.position += self.vitesse
+                self.picture('NoelD.gif')
 
     def xless(self):
         if self.win != False:
-            self.position -= self.vitesse
-            self.picture('NoelG.gif')
+            if self.gauche:
+                self.position -= self.vitesse
+                self.picture('NoelG.gif')
+
+    def wall(self):
+        if self.air_pn()[0] <= 0:
+            self.gauche = False
+        else: 
+            self.gauche = True
+        if self.air_pn()[1] >= 800:
+            self.droit = False
+        else:
+            self.droit = True 
 
     def animation(self):
         i=0
@@ -153,6 +169,7 @@ class game:
             self.balle.append(self.Fond.create_image(self.X[i],self.Y[i],image=self.photoballe[self.r[i]-2]))
         self.time()
         self.heart()
+        self.wall()
         self.fenetre.after(50,self.animation)
 
     def picture(self,url):
@@ -170,7 +187,6 @@ class game:
             return 1
         return 0
 
-
     def game_over(self,i): 
         test = self.collision(i)
         self.win -= test
@@ -185,8 +201,12 @@ class game:
             self.efface=True
         if self.win == False:
             self.btn_retry = bttn(self.fenetre,400,300,"retry","#eeeeee","#000000",self.retry)
-            if round(time.time())-self.time_d > import_data('score.json'):
-                give_data('score.json',round(time.time())-self.time_d)
+            if self.mode:
+                if round(time.time())-self.time_d > import_data('score.json'):
+                    give_data('score.json',round(time.time())-self.time_d)
+            else:
+                bttn(self.fenetre,400,330,"Perdu","#eeeeee","#000000")
+
 
     def retry(self):
         self.btn_retry.destroy()
@@ -196,19 +216,46 @@ class game:
     def time(self):
         if self.win != False:
             self.btn_time.destroy()
-            self.btn_time = bttn(self.fenetre,1,1,str(round(time.time())-self.time_d),"#eeeeee","#000000")
+            if self.mode:
+                self.btn_time = bttn(self.fenetre,1,1,str(round(time.time())-self.time_d),"#eeeeee","#000000")
+            else:
+                self.btn_time = bttn(self.fenetre,1,1,str(60-(round(time.time())-self.time_d)),"#eeeeee","#000000")
+                if 60-(round(time.time())-self.time_d) <= 0:
+                    self.btn_retry = bttn(self.fenetre,400,300,"retry","#eeeeee","#000000",self.retry)
+                    bttn(self.fenetre,400,330,"Gagné","#eeeeee","#000000")
+                    self.win = False
+
 
     def heart(self):
         self.btn_heart.destroy()
         self.btn_heart = bttn(self.fenetre,750,1,str(self.win),"#eeeeee","#000000")
 
-        
-            
-        
-
 
 
 def main():
+    """
+    Création de la première fenêtre des choix des menus
+    """
+    #####################CONFIGURATION#########################
+    global screen
+    screen = Tk()
+    screen.geometry('5x111')
+    screen.configure(bg="#141414")
+    #####################BOUTONS#########################
+    bttn(screen,0,0,"LANCER","#ffcc66","#141414",launch)
+    bttn(screen,0,37,"SURVIE","#ffcc66","#141414",launchS)
+    bttn(screen,0,74,"QUITTER","#f86263","#141414",screen.destroy)
+    ##############################################
+    screen.mainloop()
+
+
+
+def launch():
+    screen.destroy()
     game()
 
+def launchS():
+    screen.destroy()
+    game(1)
+  
 main()
